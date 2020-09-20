c0a346fd304ea0fde60c19bb72b14c4dade3133d
manikandanNeo <58359313+manikandanNeo@users.noreply.github.com>
2020-09-20 22:27:59 +0530

    image change

--- js/custom.js
+++ js/custom.js
@@ -16,7 +16,7 @@
   });
 
   $(window).scroll(function () {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > -1) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
